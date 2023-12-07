bd07f498b2cb530ec749008405aaabf49f12b300
whiteNib <139529654+whiteNib@users.noreply.github.com>
2023-12-07 16:52:16 +0900

    Add files via upload

---
+++ checkBox/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace checkBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanelBits = new System.Windows.Forms.TableLayoutPanel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Font = new System.Drawing.Font("맑은 고딕", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxResult.Location = new System.Drawing.Point(79, 161);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(498, 45);
+            this.textBoxResult.TabIndex = 0;
+            // 
+            // flowLayoutPanelBits
+            // 
+            this.flowLayoutPanelBits.ColumnCount = 16;
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.flowLayoutPanelBits.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.flowLayoutPanelBits.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanelBits.Location = new System.Drawing.Point(0, 284);
+            this.flowLayoutPanelBits.Name = "flowLayoutPanelBits";
+            this.flowLayoutPanelBits.RowCount = 4;
+            this.flowLayoutPanelBits.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.flowLayoutPanelBits.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.flowLayoutPanelBits.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.flowLayoutPanelBits.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.flowLayoutPanelBits.Size = new System.Drawing.Size(1137, 340);
+            this.flowLayoutPanelBits.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 624);
+            this.Controls.Add(this.flowLayoutPanelBits);
+            this.Controls.Add(this.textBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TableLayoutPanel flowLayoutPanelBits;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ checkBox/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checkBox
+{
+    public partial class Form1 : Form
+    {
+        private CheckBox[] bitCheckBoxes;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBitCheckBoxes();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+         private void InitializeBitCheckBoxes()
+        {
+            // 비트 수 설정
+            int bitCount = 64;
+
+            bitCheckBoxes = new CheckBox[bitCount];
+
+            for (int i = 0; i < bitCount; i++)
+            {
+                // CheckBox 생성
+                CheckBox checkBox = new CheckBox();
+                checkBox.Text = "0";
+                checkBox.CheckedChanged += BitCheckBox_CheckedChanged;
+
+                // 폼에 추가
+                flowLayoutPanelBits.Controls.Add(checkBox);
+
+                // 배열에 저장
+                bitCheckBoxes[i] = checkBox;
+                checkBox.Appearance = Appearance.Button;
+                checkBox.FlatStyle = FlatStyle.Flat;
+                checkBox.FlatAppearance.BorderSize = 0;
+                checkBox.Anchor = (AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Left);
+               
+            }
+        }
+
+        private void BitCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            // CheckBox가 변경되었을 때 처리할 로직 추가
+            UpdateResult();
+        }
+
+        private void UpdateResult()
+        {
+            // 선택된 비트를 이용한 계산 로직 추가
+            /** 예: 선택된 비트를 이용하여 2진수 값을 계산하여 표시*/
+            int result = 0;
+            for (int i = 0; i < bitCheckBoxes.Length; i++)
+            {
+                if (bitCheckBoxes[i].Checked)
+                {
+                    result |= (1 << i);
+                }
+            }
+
+            // 계산 결과를 텍스트 상자에 표시
+            textBoxResult.Text = result.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
